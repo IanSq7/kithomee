d83a4905bcf8c3e55c950bf824fbf8de6a37bed6
AlegriaBarde <127134211+AlegriaBarde@users.noreply.github.com>
2023-06-17 17:55:29 -0400

    Views y Urls Agregados.

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, about, adminvista,banqueta,changepassword,checkit,comoda,contact,lampara,lamparatecho,perfil,recovery,register,shop,silla,sitial,sofa1,sofaredondo,team
+from .views import home, about, adminvista,banqueta,changepassword,checkit,comoda,contact,lampara,lamparatecho,perfil,recovery,register,shop,silla,sitial,sofa1,sofaredondo,team,sofahogar,sofahogardetalle,escritorios,escritoriodetalle,escritoriodetalle1,roperos,roperodetalle1,roperodetalle2,sillasoficinas,sillaoficinadetalle1,sillaoficinadetalle2,veladores,veladordetalle1,veladordetalle2
 urlpatterns = [
     path('',home,name="home"),
     path('about/',about,name="about"),
@@ -20,7 +20,21 @@ urlpatterns = [
     path('sitial/',sitial,name="sitial"),
     path('sofa1/',sofa1,name="sofa1"),
     path('sofaredondo/',sofaredondo,name="sofaredondo"),
-    path('team/',team,name="team")
+    path('team/',team,name="team"),
+    path('sofahogar/',sofahogar,name="sofahogar"),
+    path('sofahogardetalle/',sofahogardetalle,name="sofahogardetalle"),
+    path('escritorios/',escritorios,name="escritorios"),
+    path('escritoriodetalle/',escritoriodetalle,name="escritoriodetalle"),
+    path('escritoriodetalle1/',escritoriodetalle1,name="escritoriodetalle1"),
+    path('roperos/',roperos,name="roperos"),
+    path('roperodetalle1/',roperodetalle1,name="roperodetalle1"),
+    path('roperodetalle2/',roperodetalle2,name="roperodetalle2"),
+    path('sillasoficinas/',sillasoficinas,name="sillasoficinas"),
+    path('sillaoficinadetalle1/',sillaoficinadetalle1,name="sillaoficinadetalle1"),
+    path('sillaoficinadetalle2/',sillaoficinadetalle2,name="sillaoficinadetalle2"),
+    path('veladores/',veladores,name="veladores"),
+    path('veladordetalle1/',veladordetalle1,name="veladordetalle1"),
+    path('veladordetalle2/',veladordetalle2,name="veladordetalle2")
 
 
 ]
--- menu/views.py
+++ menu/views.py
@@ -73,6 +73,47 @@ def sofaredondo (request):
 def team (request):
     return render (request, 'menu/team.html')
 
+def sofahogar (request):
+    return render (request, 'menu/sofahogar.html')
+
+def sofahogardetalle (request):
+    return render (request, 'menu/sofahogardetalle.html')
+
+def escritorios (request):
+    return render (request, 'menu/escritorios.html')
+
+def escritoriodetalle1 (request):
+    return render (request, 'menu/escritoriodetalle1.html')
+
+def escritoriodetalle2 (request):
+    return render (request, 'menu/escritoriodetalle2.html')
+
+def roperos (request):
+    return render (request, 'menu/roperos.html')
+
+def roperodetalle1 (request):
+    return render (request, 'menu/roperodetalle1.html')
+
+def roperodetalle2 (request):
+    return render (request, 'menu/roperodetalle2.html')
+
+def sillasoficina (request):
+    return render (request, 'menu/sillasoficina.html')
+
+def sillaoficinadetalle1 (request):
+    return render (request, 'menu/sillaoficinadetalle1.html')
+
+def sillaoficinadetalle2 (request):
+    return render (request, 'menu/sillaoficinadetalle2.html')
+
+def veladores (request):
+    return render (request, 'menu/veladores.html')
+
+def veladordetalle1 (request):
+    return render (request, 'menu/veladordetalle1.html')
+
+def veladordetalle2 (request):
+    return render (request, 'menu/veladordetalle2.html')
 # inicio se sesion / cierre de sesion
 
 
