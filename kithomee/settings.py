51bc76c7053221e072881aae47eb4e97bc4a52f3
IanSq7 <127134247+IanSq7@users.noreply.github.com>
2023-06-26 22:51:19 -0400

    setting

--- kithomee/settings.py
+++ kithomee/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'kithomee.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.oracle",
-        "NAME": "127.0.0.1:1521/oracle",
+        "NAME": "127.0.0.1:1521/orcl",
         "USER": "kithomee",
         "PASSWORD": "kt123456789",
         'TEST':{
